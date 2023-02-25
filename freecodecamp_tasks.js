a9580ba52a6e26e3599d2d6c4481c0fa1da92de8
Dorka <hdwdart@gmail.com>
2023-02-25 20:19:44 +0100

    in 102 new code

--- freecodecamp_tasks.js
+++ freecodecamp_tasks.js
@@ -99,12 +99,12 @@ const recordCollection = {
         records[id][prop] = value;
     }
     else if ( prop === "tracks" && records[id].hasOwnProperty("tracks") == false) {
-        records[id][prop] = value;
+        records[id][prop] = [value];
     }
     else if ( prop === "tracks" && value !== "") {
         records[id][prop].push(value);
     }
-    else if ( value ==="") {
+    else if ( value ==="" ) {
         delete records[id][prop];
     }
 
