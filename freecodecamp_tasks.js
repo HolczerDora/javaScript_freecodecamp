c7a2b516a7f2506067608f4d8318b5d9ecd26a6b
Dorka <hdwdart@gmail.com>
2023-02-25 14:30:31 +0100

    new data

---
+++ freecodecamp_tasks.js
@@ -0,0 +1,71 @@
+//console.log("Complex code with array and objects");
+
+/*const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    
+    "artist": "Yiruma",
+    "title": "Love",
+    "release_year": 2010,
+    "formats": [
+      "CD",
+      "MP3",
+      "MP4",
+      "CD"
+    ],
+    "gold": true
+  }
+];*/
+
+//nested object
+/*const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+console.log(gloveBoxContents)*/
+
+//nested arrays
+
+/* const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+console.log(secondTree)*/
+
+
+
