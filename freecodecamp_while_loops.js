a8b4a8944101771f3e17bfd0906f12352df677aa
Dorka <hdwdart@gmail.com>
2023-02-27 14:07:42 +0100

    new for loops (i+=2 ; i-=2)

--- freecodecamp_while_loops.js
+++ freecodecamp_while_loops.js
@@ -25,8 +25,24 @@ for (let i = 0; i < 5; i++) {
   ourArray will now have the value [0, 1, 2, 3, 4].
 } */
 
-const myArray = [];
+/*const myArray = [];
 for ( let i = 1; i <= 5; i++) {
     myArray.push(i);
 }
-//[1,2,3,4,5]
+//[1,2,3,4,5] */
+
+//task: Push the odd numbers from 1 through 9 to myArray using a for loop. [1,3,5,7,9]
+
+const myArray = [];
+for (let i = 1; i <= 9; i+=2) {
+  myArray.push(i);
+}
+
+//Push the odd numbers from 9 through 1 to myArray using a for loop. [9,7,5,3,1]
+
+const myArray = [];
+for ( let i = 9; i >= 1; i-=2) {
+  myArray.push(i);
+}
+
+
