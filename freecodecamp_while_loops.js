b5a89eee3f6ecec45e2a37b6f2065376264dc765
Dorka <hdwdart@gmail.com>
2023-02-27 08:56:29 +0100

    new for loop [1,2,3,4,5]

--- freecodecamp_while_loops.js
+++ freecodecamp_while_loops.js
@@ -8,11 +8,25 @@ while (i < 5) {
 
 /* task: Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.*/
 
-const myArray = [];
+/*const myArray = [];
 let i = 5;
 
 while ( i >= 0) {
     myArray.push (i);
     i--;
-}
+} */
 
+/* for loop
+const ourArray = [];
+
+for (let i = 0; i < 5; i++) {
+  ourArray.push(i);
+
+  ourArray will now have the value [0, 1, 2, 3, 4].
+} */
+
+const myArray = [];
+for ( let i = 1; i <= 5; i++) {
+    myArray.push(i);
+}
+//[1,2,3,4,5]
